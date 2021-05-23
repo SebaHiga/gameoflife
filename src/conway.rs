dc67a1b7f883f8aa38d07341738377e17efcfab7
sebhig <seb.higa@outlook.com>
2021-05-23 16:09:43 -0300

    cownay in a serpare module

--- src/conway.rs
+++ src/conway.rs
@@ -1,5 +1,81 @@
-use cell::{Cell, Status};
+use std::io::{stdout, Write};
 
-struct Conway {
-    matrix: Vec<&Vec<Cell>>,
+use rand::prelude::*;
+use termion::{raw::IntoRawMode, terminal_size};
+#[derive(Clone, Copy, std::cmp::PartialEq)]
+pub enum Status {
+    Alive,
+    Dead,
+}
+
+impl Status {
+    pub fn get_rand() -> Status {
+        let mut rng = thread_rng();
+        if rng.gen_range(0, 10) > 8 {
+            return Status::Alive;
+        }
+
+        Status::Dead
+    }
+}
+
+#[derive(Copy, Clone)]
+pub struct Cell {
+    pub content: char,
+    pub status: Status,
+}
+
+impl Cell {
+    pub fn get_status(&self) -> Status {
+        self.status
+    }
+
+    pub fn get_content(&self) -> char {
+        self.content
+    }
+}
+
+pub struct GameOfLife {
+    pub matrix: Vec<Vec<Cell>>,
+}
+
+impl GameOfLife {
+    pub fn render(&self) {
+        let mut stdout = stdout().into_raw_mode().unwrap();
+        let (row, col) = terminal_size().unwrap();
+
+        write!(
+            stdout,
+            "{clear}{goto}",
+            clear = termion::clear::All,
+            goto = termion::cursor::Goto(1, 1)
+        )
+        .unwrap();
+
+        for index_row in 0..self.matrix.len() {
+            for index_col in 0..self.matrix[index_row].len() {
+                let cell = self.matrix[index_row][index_col].clone();
+
+                if cell.get_status() == Status::Alive {
+                    write!(
+                        stdout,
+                        "{}",
+                        self.matrix[index_row][index_col].get_content()
+                    )
+                    .unwrap();
+                } else {
+                    write!(stdout, "{}", ' ').unwrap();
+                }
+
+                stdout.flush().unwrap();
+
+                if index_col >= (row - 1) as usize {
+                    break;
+                }
+            }
+            if index_row >= (col - 1) as usize {
+                break;
+            }
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,12 @@
-use std::{
-    io::{stdout, Read, Write},
-    u16,
-};
+use std::{io::Read, thread, time};
 
-use std::{thread, time};
-use termion::{
-    async_stdin,
-    raw::{IntoRawMode, RawTerminal},
-    terminal_size,
-};
+use termion::async_stdin;
 
-use cell::{Cell, Status};
-pub mod cell;
-
-fn render(stdout: &mut RawTerminal<std::io::Stdout>, matrix: &Vec<Vec<Cell>>, row: u16, col: u16) {
-    for index_row in 0..matrix.len() {
-        for index_col in 0..matrix[index_row].len() {
-            let cell = matrix[index_row][index_col].clone();
-
-            if cell.get_status() == Status::Alive {
-                write!(stdout, "{}", matrix[index_row][index_col].get_content()).unwrap();
-            } else {
-                write!(stdout, "{}", ' ').unwrap();
-            }
-
-            stdout.flush().unwrap();
-
-            if index_col >= (row - 1) as usize {
-                break;
-            }
-        }
-        if index_row >= (col - 2) as usize {
-            break;
-        }
-    }
-}
+mod conway;
+use conway::{Cell, GameOfLife, Status};
 
 fn main() {
     let mut stdin = async_stdin().bytes();
-    let mut stdout = stdout().into_raw_mode().unwrap();
     let fps = 12.0;
     let ten_millis = time::Duration::from_secs_f32(1.0 / fps);
     let mut matrix = Vec::new();
@@ -48,8 +16,6 @@ fn main() {
 
         for _ in 0..256 {
             cells.push(Cell {
-                row: 0,
-                col: 0,
                 content: '█',
                 status: Status::get_rand(),
             });
@@ -58,26 +24,19 @@ fn main() {
         matrix.push(cells);
     }
 
+    let gol = GameOfLife { matrix: matrix };
+
     loop {
-        let (row, col) = terminal_size().unwrap();
         let b = stdin.next();
 
-        write!(
-            stdout,
-            "{clear}{goto}{x}, {y}\n\r",
-            clear = termion::clear::All,
-            goto = termion::cursor::Goto(1, 1),
-            x = row,
-            y = col
-        )
-        .unwrap();
-        stdout.flush().unwrap();
-
-        if let Some(Ok(b'q')) = b {
+        // temporal manual next
+        if let Some(Ok(b'n')) = b {
+            gol.render();
+        } else if let Some(Ok(b'q')) = b {
             break;
         }
 
-        render(&mut stdout, &matrix, row, col);
+        gol.render();
 
         thread::sleep(ten_millis);
     }
